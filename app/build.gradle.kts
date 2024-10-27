8dbf44652dc1be5ee0ccfd46f890da6ab9422c0e
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-10-27 22:01:06 +0700

    update course

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.viewpager2:viewpager2:1.1.0")
+    implementation ("com.google.android.material:material:1.12.0")
 }
