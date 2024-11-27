95f3dd1ffecb1a2df3ffd0a6767377f33dcc2c40
Nguyễn Văn Vũ <vanvu101204@gmail.com>
2024-11-28 06:10:53 +0700

    commit fix sua bai viet

--- app/src/main/java/com/snapco/techlife/data/api/ApiClient.kt
+++ app/src/main/java/com/snapco/techlife/data/api/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://26.187.200.144:3000/"
+    private const val BASE_URL = "http://192.168.0.105:3000/"
 
     private val logging =
         HttpLoggingInterceptor().apply {
