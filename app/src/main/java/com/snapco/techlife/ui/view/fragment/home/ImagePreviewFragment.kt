69e49deea26e8e21e69cf685261ddfde74d685f5
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-12-03 22:53:34 +0700

    commit previewImage

--- app/src/main/java/com/snapco/techlife/ui/view/fragment/home/ImagePreviewFragment.kt
+++ app/src/main/java/com/snapco/techlife/ui/view/fragment/home/ImagePreviewFragment.kt
@@ -51,10 +51,14 @@ class ImagePreviewDialogFragment : DialogFragment() {
         )
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.BLACK))
         dialog.window?.statusBarColor = Color.BLACK
+        
+        dialog.window?.decorView?.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_FULLSCREEN
 
         dialog.setCanceledOnTouchOutside(true)
 
         return dialog
     }
+
 }
 
