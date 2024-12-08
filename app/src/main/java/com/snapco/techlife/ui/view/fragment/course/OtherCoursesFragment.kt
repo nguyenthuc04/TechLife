5314f9e96a12fbb64e1daa3f9333ae9f03234974
tatuan04 <130113809+tatuan04@users.noreply.github.com>
2024-12-08 20:40:26 +0700

    sua giao dien

--- app/src/main/java/com/snapco/techlife/ui/view/fragment/course/OtherCoursesFragment.kt
+++ app/src/main/java/com/snapco/techlife/ui/view/fragment/course/OtherCoursesFragment.kt
@@ -4,34 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.snapco.techlife.data.model.Course
-import com.snapco.techlife.data.model.UpdateCourseRequest
-import com.snapco.techlife.data.model.UserCourse
 import com.snapco.techlife.databinding.FragmentOtherCoursesBinding
 import com.snapco.techlife.extensions.replaceFragment
 import com.snapco.techlife.extensions.startActivity
-import com.snapco.techlife.extensions.visible
 import com.snapco.techlife.ui.view.adapter.OtherCourseAdapter
 import com.snapco.techlife.ui.viewmodel.CourseViewModel
-import com.snapco.techlife.ui.viewmodel.SearchViewModel
 import com.snapco.techlife.ui.viewmodel.objectdataholder.DataCheckMart
-import com.snapco.techlife.ui.viewmodel.objectdataholder.UserDataHolder
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import io.getstream.chat.android.models.Filters
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.IsoFields
@@ -41,39 +28,22 @@ class OtherCoursesFragment : Fragment() {
     private val courseActivityViewModel: CourseViewModel by activityViewModels()
 
     private lateinit var binding: FragmentOtherCoursesBinding
-    private lateinit var otherCourseAdapter: OtherCourseAdapter
+    private lateinit var basicCourseAdapter: OtherCourseAdapter
+    private lateinit var advancedCourseAdapter: OtherCourseAdapter
+    private lateinit var specializedCourseAdapter: OtherCourseAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         binding = FragmentOtherCoursesBinding.inflate(inflater, container, false)
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        // Khởi tạo adapter với callback xử lý nút
-        otherCourseAdapter = OtherCourseAdapter(
-            mutableListOf(),
-            onItemClickListener = { course ->
-                // Xử lý khi bấm vào item
-                courseActivityViewModel.setCours(course)
-                replaceFragment(CourseDetailsFragment())
-            },
-        )
-
-        binding.imgFilter.setOnClickListener {
-            startActivity<CourseFilterActivity>()
-        }
-
-        binding.recyclerView.adapter = otherCourseAdapter
+        setupRecyclerViews()
 
         courseViewModel.getListCourses()
-
         courseViewModel.courses.observe(viewLifecycleOwner) { courseList ->
-//            otherCourseAdapter.updateCourses(courseList)
-            getDataFilter(courseList)
+            filterAndDisplayCourses(courseList)
         }
 
         binding.edtSearchCourse.addTextChangedListener(object : TextWatcher {
@@ -83,10 +53,9 @@ class OtherCoursesFragment : Fragment() {
                 val query = s.toString().trim()
                 courseViewModel.searchCourses(query)
                 courseViewModel.searchCourses.observe(viewLifecycleOwner) { courseList ->
-//                    otherCourseAdapter.updateCourses(courseList)
-                    getDataFilter(courseList)
+                    filterAndDisplayCourses(courseList)
                 }
-                if(query.isEmpty()){
+                if (query.isEmpty()) {
                     courseViewModel.getListCourses()
                 }
             }
@@ -94,15 +63,123 @@ class OtherCoursesFragment : Fragment() {
             override fun afterTextChanged(s: Editable?) {}
         })
 
+        binding.imgFilter.setOnClickListener {
+            startActivity<CourseFilterActivity>()
+        }
+
         return binding.root
     }
 
+    private fun setupRecyclerViews() {
+        basicCourseAdapter =
+            OtherCourseAdapter(mutableListOf()) { course -> onCourseClicked(course) }
+        advancedCourseAdapter =
+            OtherCourseAdapter(mutableListOf()) { course -> onCourseClicked(course) }
+        specializedCourseAdapter =
+            OtherCourseAdapter(mutableListOf()) { course -> onCourseClicked(course) }
+
+        binding.rvBasicCourses.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = basicCourseAdapter
+        }
+
+        binding.rvAdvancedCourses.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = advancedCourseAdapter
+        }
+
+        binding.rvSpecializedCourses.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = specializedCourseAdapter
+        }
+    }
+
+    private fun filterAndDisplayCourses(courseList: List<Course>) {
+        val retrievedOptions = DataCheckMart.getMultiplePreferences(requireContext(), "MyChecked")
+        val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+        retrievedOptions?.let {
+            val filteredCourses = courseList.filter { course ->
+                var matches = true
+
+                for (option in it) {
+                    when (option) {
+                        "Mức độ liên quan", "Bất kỳ", "Mọi thời điểm", "Tất cả" -> matches
+                        "Dưới 2 tiếng" -> matches =
+                            matches && course.duration.replace(Regex("[^\\d]"), "").toInt() < 2
+
+                        "2 - 5 tiếng" -> matches =
+                            matches && course.duration.replace(Regex("[^\\d]"), "").toInt() in 2..5
+
+                        "Trên 5 tiếng" -> matches =
+                            matches && course.duration.replace(Regex("[^\\d]"), "").toInt() > 5
+
+                        "Hôm nay" -> {
+                            val today = LocalDate.now()
+                            val startDate = LocalDate.parse(course.startDate, dateFormatter)
+                            matches = matches && startDate.isEqual(today)
+                        }
+
+                        "Tuần này" -> {
+                            val today = LocalDate.now()
+                            val weekOfYearToday = today.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
+                            val startDate = LocalDate.parse(course.startDate, dateFormatter)
+                            val weekOfYearStart = startDate.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
+                            matches = matches && weekOfYearStart == weekOfYearToday
+                        }
+
+                        "Tháng này" -> {
+                            val today = LocalDate.now()
+                            val monthToday = today.monthValue
+                            val startDate = LocalDate.parse(course.startDate, dateFormatter)
+                            val monthStart = startDate.monthValue
+                            matches = matches && monthStart == monthToday
+                        }
+
+                        "Cơ bản" -> matches = matches && course.type == "Cơ bản"
+                        "Nâng cao" -> matches = matches && course.type == "Nâng cao"
+                        "Chuyên môn hoá" -> matches = matches && course.type == "Chuyên môn hoá"
+                    }
+                }
+                matches
+            }
+
+            if (filteredCourses.isEmpty()) {
+                binding.tvNoCourses.visibility = View.VISIBLE
+            } else {
+                binding.tvNoCourses.visibility = View.GONE
+            }
+
+            val basicCourses = filteredCourses.filter { it.type == "Cơ bản" }
+            val advancedCourses = filteredCourses.filter { it.type == "Nâng cao" }
+            val specializedCourses = filteredCourses.filter { it.type == "Chuyên môn hoá" }
+
+            basicCourseAdapter.updateCourses(basicCourses)
+            advancedCourseAdapter.updateCourses(advancedCourses)
+            specializedCourseAdapter.updateCourses(specializedCourses)
+
+            binding.tvBasicCourses.visibility =
+                if (basicCourses.isEmpty()) View.GONE else View.VISIBLE
+            binding.rvBasicCourses.visibility =
+                if (basicCourses.isEmpty()) View.GONE else View.VISIBLE
+            binding.tvAdvancedCourses.visibility =
+                if (advancedCourses.isEmpty()) View.GONE else View.VISIBLE
+            binding.rvAdvancedCourses.visibility =
+                if (advancedCourses.isEmpty()) View.GONE else View.VISIBLE
+            binding.tvSpecializedCourses.visibility =
+                if (specializedCourses.isEmpty()) View.GONE else View.VISIBLE
+            binding.rvSpecializedCourses.visibility =
+                if (specializedCourses.isEmpty()) View.GONE else View.VISIBLE
+        }
+    }
+
+    private fun onCourseClicked(course: Course) {
+        courseActivityViewModel.setCours(course)
+        replaceFragment(CourseDetailsFragment())
+    }
+
     @Deprecated("Deprecated in Java")
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?,
-    ) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_ADD_COURSE && resultCode == AppCompatActivity.RESULT_OK) {
             courseViewModel.getListCourses()
@@ -113,95 +190,11 @@ class OtherCoursesFragment : Fragment() {
         private const val REQUEST_CODE_ADD_COURSE = 1
     }
 
-    fun getDataFilter (list : List<Course>) {
-        val retrievedOptions = DataCheckMart.getMultiplePreferences(requireContext(), "MyChecked")
-        val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-
-        retrievedOptions?.let {
-            val filteredCourses = list.filter { course ->
-                var matches = true
-
-                // Kiểm tra các tùy chọn trong retrievedOptions và lọc
-                for (option in it) {
-                    when (option) {
-                        "Mức độ liên quan" -> {
-                            matches
-                        }
-                        "Bất kỳ" -> {
-                            // Lọc theo "Bất kỳ" (có thể không cần điều kiện gì)
-                            matches
-                        }
-                        "Dưới 2 tiếng" -> {
-                            // Lọc theo thời gian khóa học dưới 2 tiếng
-                            matches = matches && course.duration.toInt() < 2
-                        }
-                        "2 - 5 tiếng" -> {
-                            // Lọc theo thời gian khóa học từ 2 đến 5 tiếng
-                            matches = matches && course.duration.toInt() in 2..5
-                        }
-                        "Trên 5 tiếng" -> {
-                            // Lọc theo thời gian khóa học trên 5 tiếng
-                            matches = matches && course.duration.toInt() > 5
-                        }
-                        "Mọi thời điểm" -> {
-                            // Lọc theo "Mọi thời điểm"
-                            matches
-                        }
-                        "Hôm nay" -> {
-                            // Lọc theo "Hôm nay"
-                            val today = LocalDate.now()
-                            val startDate = LocalDate.parse(course.startDate, dateFormatter) // Dùng formatter
-                            matches = matches && startDate.isEqual(today)
-                        }
-                        "Tuần này" -> {
-                            // Lọc theo "Tuần này"
-                            val today = LocalDate.now()
-                            val weekOfYearToday = today.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
-                            val startDate = LocalDate.parse(course.startDate, dateFormatter)
-                            val weekOfYearStart = startDate.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
-                            matches = matches && weekOfYearStart == weekOfYearToday
-                        }
-                        "Tháng này" -> {
-                            // Lọc theo "Tháng này"
-                            val today = LocalDate.now()
-                            val monthToday = today.monthValue
-                            val startDate = LocalDate.parse(course.startDate, dateFormatter)
-                            val monthStart = startDate.monthValue
-                            matches = matches && monthStart == monthToday
-                        }
-                        "Tất cả" -> {
-                            // Lọc theo "Tất cả"
-                            matches
-                        }
-                        "Cơ bản" -> {
-                            // Lọc theo "Cơ bản"
-                            matches = matches && course.type == "Cơ bản"
-                        }
-                        "Nâng cao" -> {
-                            // Lọc theo "Nâng cao"
-                            matches = matches && course.type == "Nâng cao"
-                        }
-                        "Chuyên môn hoá" -> {
-                            // Lọc theo "Chuyên môn hoá"
-                            matches = matches && course.type == "Chuyên môn hoá"
-                        }
-                    }
-                }
-
-                // Trả về kết quả lọc
-                matches
-            }
-            otherCourseAdapter.updateCourses(filteredCourses)
-        }
-    }
-
     override fun onResume() {
         courseViewModel.getListCourses()
         courseViewModel.courses.observe(viewLifecycleOwner) { courseList ->
-            getDataFilter(courseList) // Áp dụng các bộ lọc cho danh sách khóa học
+            filterAndDisplayCourses(courseList)
         }
         super.onResume()
     }
-
-
 }
